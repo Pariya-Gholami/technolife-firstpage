baf59ed1a6e716f029f69bd47ac2953d107464c5
pariya <pariyagholami152@gmail.com>
2025-11-08 15:25:22 +0330

    t

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:technolife-firstpage,
+  base:'/technolife-firstpage/',
 
   plugins: [
     tailwindcss(),
